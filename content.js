a2f841df471b882018c5be27daa68efff0eecf48
0ZMilad <Milad.Amini2026@gmail.com>
2024-07-01 16:50:53 +0100

    Improve content.js logic

--- content.js
+++ content.js
@@ -4,12 +4,12 @@ console.log("Content script loaded.");
 // Upcoming functionality: Scrape ingredients from webpage and identify harmful ones
 
 // Steps to implement:
-// 1. Scrape item ingredients from the webpage, specifically for items added to the shopping cart.
+// 1. Scrape item ingredients from the webpage using scrape logic code in server. (Completed main logic however need post request to send the website url to the server (Amazon, Amazon Fresh) and express get request to get data from the server)
 // 2. Cross-reference each item's ingredients against a predefined list of harmful ingredients.
 // 3. If a match is found, flag the ingredient as harmful.
 // 4. Display a notification to the user indicating which item contains the harmful ingredient.
-// 5. Provide an option for the user to remove the flagged item from their cart.
+// 5. Provide an option for the user to remove the flagged item from their cart (If its added to the cart).
 
 // This outlines the planned approach for identifying and alerting users about harmful ingredients in their shopping cart items.
 
-// Additonally in the future an ingredient API could be used to provide more detailed information about the harmful ingredients.
+// Additonally in the future an ingredient API could be used to provide more detailed information about the harmful ingredients and nofitfy users of the items in the cart with harmful ingredients.
