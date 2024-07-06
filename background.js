0e67b14601512bd47b925229c4e86777f36c45c8
0ZMilad <Milad.Amini2026@gmail.com>
2024-07-06 22:28:27 +0100

    Add listener for messages from content script to send URL to server

--- background.js
+++ background.js
@@ -2,3 +2,28 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("AllergyGuard for Groceries installed.");
 });
+
+// Message received from content script  - add listener to listen for message from content script
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "send_url") {
+    // Post request to server
+    fetch("http://localhost:3000/scrape", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: request.url }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        sendResponse({ message: "URL sent to server." });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return true;
+  }
+});
