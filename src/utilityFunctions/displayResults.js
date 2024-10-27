356f44eb2bd39b67185b5f931a77dee287c4c21e
0ZMilad <Milad.Amini2026@gmail.com>
2024-10-27 10:57:03 +0000

    Truncate long item names in alert messages

--- src/utilityFunctions/displayResults.js
+++ src/utilityFunctions/displayResults.js
@@ -1,15 +1,31 @@
+// Function to shorten long item names
+function shortenItemName(name, maxLength) {
+    if (name.length > maxLength) {
+        return name.substring(0, maxLength) + '...';
+    } else {
+        return name;
+    }
+}
+
 // Function to display results to the user
 function displayResults(itemName, matchedIngredients) {
     const badIngredientsArray = [...matchedIngredients];
 
+    const maxItemNameLength = 20; // Max length for item names
+
+    // Shorten long item names
+    const displayedItemName = shortenItemName(itemName, maxItemNameLength);
+
     if (badIngredientsArray.length > 0) {
-        const alertMessage = `Warning: The product ${itemName} contains the following concerning ingredients:\n\n${badIngredientsArray.join(
+        const alertMessage = `Warning: The product ${displayedItemName} contains the following concerning ingredients:\n\n${badIngredientsArray.join(
             ', '
         )}`;
         console.log('Displaying dialog with message:', alertMessage);
         createDialog(alertMessage);
     } else {
-        console.log(`No concerning ingredients found in "${itemName}".`);
+        console.log(
+            `No concerning ingredients found in "${displayedItemName}".`
+        );
     }
 }
 
